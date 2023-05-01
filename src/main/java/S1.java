1ec437744d07c6defd45dbef54da74dc352d7ad3
lemonsauce21 <lemonsauce21@gmail.com>
2023-05-01 22:47:55 +0900

    commit tttt

---
+++ src/main/java/S1.java
@@ -0,0 +1,12 @@
+public class S1 {
+    public static void main(String[] args){
+        solution();
+    }
+
+    private static void solution(){
+        System.out.println("chk");
+    }
+    public static void solution2(){
+        System.out.println("commit test");
+    }
+}
